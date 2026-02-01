86c0f89fc9f6f9cbb9ac84d986656a723680fe36
Gabriela <g.grasmane@ucr.nl>
2026-02-01 19:35:30 +0100

    create files

--- model.py
+++ model.py
@@ -1,2 +1,9 @@
-# Model definition
+# Import libraries
 
+import numpy as np, scipy as sp, pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import networkx as nx
+import mesa as ms
+
+# Define model classes and functions
--- run.py
+++ run.py
@@ -1,1 +1,1 @@
-# Test run
+# Run the model
